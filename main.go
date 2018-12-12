19f83a8ee81fa81c496b40e827bb6c7e54672c1a
Christian Roese <croese@gmail.com>
2018-12-11 20:29:24 -0600

    Adds chi middleware

--- main.go
+++ main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 )
+
+func Routes() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(
+		render.SetContentType(render.ContentTypeJSON), // set content-type JSON
+		middleware.RequestID,                          // adds request ID
+		middleware.Logger,                             // logs request details
+		middleware.DefaultCompress,                    // compress results
+		middleware.RedirectSlashes,                    // redirect slashes to no slash version
+		middleware.Recoverer,                          // recover from panics without crashing
+	)
+
+	router.Route("/v1/api", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("welcome"))
+		})
+	})
+
+	return router
+}
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
-	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
-	http.ListenAndServe(":"+port, r)
+
+	router := Routes()
+
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
