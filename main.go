80057d5c80c27bc481f499e94695cd5174fefe33
Christian Roese <croese@gmail.com>
2018-12-11 19:42:26 -0600

    Adds port config

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":"+port, r)
 }
